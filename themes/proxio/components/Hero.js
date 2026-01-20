744210b3adcb5dfb5a3414d1382d327265ee5425
zmz010331 <zmz010331@gmail.com>
2026-01-20 12:50:24 +0000

    主题修改

--- themes/proxio/components/Hero.js
+++ themes/proxio/components/Hero.js
@@ -45,54 +45,54 @@ export const Hero = props => {
           className='w-full absolute h-screen left-0 top-0 pointer-events-none'
         />
         {/* 阴影遮罩 */}
-        <div className='h-1/3 w-full absolute left-0 bottom-0 z-10'>
+        <div className='h-1/2 w-full absolute left-0 bottom-0 z-10'>
           <div
-            className='h-full w-full absolute group-hover:opacity-100 transition-all duration-1000 
-                    bg-gradient-to-b from-transparent to-white dark:to-black'
+            className='h-full w-full absolute bg-gradient-to-b from-transparent to-black'
           />
         </div>
-      </div>
-      {/* 文字标题等 */}
-      <div className='w-full pb-15 dark:text-white'>
-        <div className='container -mx-4 flex flex-wrap items-center'>
-          <div className='w-full px-4'>
-            <div
-              className='hero-content wow fadeInUp mx-auto max-w-[780px] text-center'
-              data-wow-delay='0.5s'>
-              {/* 主标题 */}
-              <h1 className='mb-6 text-3xl font-bold leading-snug sm:text-4xl sm:leading-snug lg:text-5xl lg:leading-[1.2]'>
-                {siteConfig('PROXIO_HERO_TITLE_1', null, config)}
-              </h1>
-              {/* 次标题 */}
-              <p className='mx-auto mb-9 max-w-[600px] text-base font-medium  sm:text-lg sm:leading-[1.44]'>
-                {siteConfig('PROXIO_HERO_TITLE_2', null, config)}
-              </p>
-              {/* 按钮组 */}
-              <ul className='mb-10 flex flex-wrap items-center justify-center gap-5'>
-                {PROXIO_HERO_BUTTON_1_TEXT && (
-                  <li>
-                    <SmartLink
-                      href={siteConfig('PROXIO_HERO_BUTTON_1_URL', '')}
-                      className='inline-flex items-center justify-center rounded-2xl bg-white px-7 py-[14px] text-center text-base font-medium text-dark shadow-1 transition duration-300 ease-in-out hover:bg-gray-2'>
-                      {PROXIO_HERO_BUTTON_1_TEXT}
-                    </SmartLink>
-                  </li>
-                )}
-                {PROXIO_HERO_BUTTON_2_TEXT && (
-                  <li>
-                    <SmartLink
-                      href={siteConfig('PROXIO_HERO_BUTTON_2_URL', '')}
-                      className='inline-flex items-center justify-center rounded-2xl bg-white px-7 py-[14px] text-center text-base font-medium text-dark shadow-1 transition duration-300 ease-in-out hover:bg-gray-2'>
-                      {PROXIO_HERO_BUTTON_2_ICON && (
-                        <img className='mr-4 w-5' src={PROXIO_HERO_BUTTON_2_ICON} />
-                      )}
-                      {PROXIO_HERO_BUTTON_2_TEXT}
-                    </SmartLink>
-                  </li>
-                )}
-              </ul>
+
+        {/* 文字标题等 */}
+        <div className='w-full absolute bottom-0 z-20 pb-15 dark:text-white'>
+            <div className='container -mx-4 flex flex-wrap items-center'>
+            <div className='w-full px-4'>
+                <div
+                className='hero-content wow fadeInUp mx-auto max-w-[780px] text-center'
+                data-wow-delay='0.5s'>
+                {/* 主标题 */}
+                <h1 className='mb-6 text-3xl font-bold leading-snug sm:text-4xl sm:leading-snug lg:text-5xl lg:leading-[1.2]'>
+                    {siteConfig('PROXIO_HERO_TITLE_1', null, config)}
+                </h1>
+                {/* 次标题 */}
+                <p className='mx-auto mb-9 max-w-[600px] text-base font-medium  sm:text-lg sm:leading-[1.44]'>
+                    {siteConfig('PROXIO_HERO_TITLE_2', 'I am a slow walker, but I never walk backwards.', config)}
+                </p>
+                {/* 按钮组 */}
+                <ul className='mb-10 flex flex-wrap items-center justify-center gap-5'>
+                    {PROXIO_HERO_BUTTON_1_TEXT && (
+                    <li>
+                        <SmartLink
+                        href={siteConfig('PROXIO_HERO_BUTTON_1_URL', '')}
+                        className='inline-flex items-center justify-center rounded-2xl bg-white px-7 py-[14px] text-center text-base font-medium text-dark shadow-1 transition duration-300 ease-in-out hover:bg-gray-2'>
+                        {PROXIO_HERO_BUTTON_1_TEXT}
+                        </SmartLink>
+                    </li>
+                    )}
+                    {PROXIO_HERO_BUTTON_2_TEXT && (
+                    <li>
+                        <SmartLink
+                        href={siteConfig('PROXIO_HERO_BUTTON_2_URL', '')}
+                        className='inline-flex items-center justify-center rounded-2xl bg-white px-7 py-[14px] text-center text-base font-medium text-dark shadow-1 transition duration-300 ease-in-out hover:bg-gray-2'>
+                        {PROXIO_HERO_BUTTON_2_ICON && (
+                            <img className='mr-4 w-5' src={PROXIO_HERO_BUTTON_2_ICON} />
+                        )}
+                        {PROXIO_HERO_BUTTON_2_TEXT}
+                        </SmartLink>
+                    </li>
+                    )}
+                </ul>
+                </div>
             </div>
-          </div>
+            </div>
         </div>
       </div>
       {/* <!-- ====== Hero Section End --> */}
